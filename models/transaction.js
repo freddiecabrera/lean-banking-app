85e4ff6b6ba3c4cca6a06a9ab2cc85a69f35cf20
freddiecabrera <freddiecabrera18@yahoo.com>
2016-03-10 10:50:37 -0800

    deploy

--- models/transaction.js
+++ models/transaction.js
@@ -14,57 +14,58 @@ exports.get = function(cb) {
   });
 };
 
-exports.create = function(newTran, cb) {
-  this.get((err, trans) => {
-    if(err){ return cb(err); }
-    newTran.id = uuid();
-    trans.push(newTran);
-    this.write(trans, cb);
-  });
-};
 
-exports.write = function(trans, cb) {
-  fs.writeFile(transactionPathFile, JSON.stringify(trans), cb);
-};
-
-
-exports.delete = function(id, cb){
-  this.get((err, trans) => {
-  if(err) return cb(err);
-
-  var transLength = trans.length;
-  var newTrans = trans.filter(function(item) {
-    return item.id !== id;
-  });
-
-  if(transLength === newTrans.length){
-    cb({err: 'Transaction not found'});
-  }
-
-  this.write(newTrans, cb);
-  });
-};
-
-exports.update = function(id, updatesObj, cb) {
-  this.get((err, trans) => {
-    if(err) return cb(err);
-
-    var updatedTran;
-    var updatedTrans = trans.map(function(item) {
-      if(item.id === id){
-        for(var key in updatesObj){
-          item[key] = updatesObj[key]
-        }
-        updatedTran = item;
-      }
-      return item;
-    });
-    if(!updatedTran){
-      cb({err: 'Shoe not found'});
-    }
-
-    this.write(updatedTrans, function(err) {
-      cb(err, updatedTran);
-    });
-  });
-};
+// exports.create = function(newTran, cb) {
+//   this.get((err, trans) => {
+//     if(err){ return cb(err); }
+//     newTran.id = uuid();
+//     trans.push(newTran);
+//     this.write(trans, cb);
+//   });
+// };
+//
+// exports.write = function(trans, cb) {
+//   fs.writeFile(transactionPathFile, JSON.stringify(trans), cb);
+// };
+//
+//
+// exports.delete = function(id, cb){
+//   this.get((err, trans) => {
+//   if(err) return cb(err);
+//
+//   var transLength = trans.length;
+//   var newTrans = trans.filter(function(item) {
+//     return item.id !== id;
+//   });
+//
+//   if(transLength === newTrans.length){
+//     cb({err: 'Transaction not found'});
+//   }
+//
+//   this.write(newTrans, cb);
+//   });
+// };
+//
+// exports.update = function(id, updatesObj, cb) {
+//   this.get((err, trans) => {
+//     if(err) return cb(err);
+//
+//     var updatedTran;
+//     var updatedTrans = trans.map(function(item) {
+//       if(item.id === id){
+//         for(var key in updatesObj){
+//           item[key] = updatesObj[key]
+//         }
+//         updatedTran = item;
+//       }
+//       return item;
+//     });
+//     if(!updatedTran){
+//       cb({err: 'Shoe not found'});
+//     }
+//
+//     this.write(updatedTrans, function(err) {
+//       cb(err, updatedTran);
+//     });
+//   });
+// };
